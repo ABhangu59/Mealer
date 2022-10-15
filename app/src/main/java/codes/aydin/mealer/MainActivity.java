49f0e8f4372960d70ed03e7813f87e09b9e3886d
tszul056 <tszul056@uottawa.ca>
2022-10-15 12:35:09 -0400

    signup page set up

--- app/src/main/java/codes/aydin/mealer/MainActivity.java
+++ app/src/main/java/codes/aydin/mealer/MainActivity.java
@@ -2,7 +2,10 @@ package codes.aydin.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView signup=(TextView)findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent launchActivity = new Intent(MainActivity.this,SignUp.class);
+               startActivity(launchActivity);
+           }
+        });
     }
 }
